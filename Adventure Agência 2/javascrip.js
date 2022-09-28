436310a0105aeb51ed0570d2d1da95854a2d6f98
marciojunior1995 <108499129+marciojunior1995@users.noreply.github.com>
2022-09-28 08:52:58 -0300

    Add files via upload

---
+++ Adventure Agência 2/javascrip.js
@@ -0,0 +1,11 @@
+function enviar(){
+
+    var nome = document.getElementById("nomeid")
+
+    if (nome.value !=""){
+        alert(`OBRIGADO SR(A) `+nome.value+` OS SEUS DADOS FORAM EMCAMINHADOS COM SUCESSO!!!`);
+    
+    }
+}
+
+
